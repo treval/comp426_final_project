fbf2097cf68fa3a9e9c3247ee5fad550b2229362
Trevor Levey <trvlev@gmail.com>
2017-12-05 15:04:57 -0500

    Drafted user/rsvp orm completely

--- server_side/orm/user_orm.php
+++ server_side/orm/user_orm.php
@@ -28,5 +28,105 @@ class User {
 		}
 		return null;
 	}
+
+	public static function findByID($id) {
+		$mysqli = User::connect();
+
+		$result = $mysqli->query("SELECT * FROM User where id = " . $id);
+		if ($result) {
+			if ($result->num_rows == 0) {
+				return null;
+			}
+
+			$user_info = $result->fetch_array();
+
+			return new User(intval($user_info['id']),
+				$user_info['first'],
+				$user_info['last'],
+				$user_info['email']);
+		}
+		return null;
+	}
+
+	public static function getAllIDs() {
+		$mysqli = User::connect();
+
+		$result = $mysqli->query("SELECT id FROM User");
+		$id_array = array();
+
+		if ($result) {
+			while ($next_row = $result->fetch_array()) {
+				$id_array[] = intval($next_row['id']);
+			}
+		}
+		return $id_array;
+	}
+
+	private function __construct($id, $first, $last, $email) {
+		$this->id = $id;
+		$this->first = $fisrt;
+		$this->last = $last;
+		$this->email = $email;
+	}
+
+	public function getID() {
+		return $this->id;
+	}
+
+	public function getFirst() {
+		return $this->title;
+	}
+
+	public function getLast() {
+		return $this->note;
+	}
+
+	public function getEmail() {
+		return $this->project;
+	}
+
+	public function setFirst($first) {
+		$this->first = $firts;
+		return $this->update();
+	}
+
+	public function setLast($last) {
+		$this->last = $last;
+		return $this->update();
+	}
+
+	public function setEmail($email) {
+		$this->email = $email;
+		return $this->update();
+	}
+
+	private function update() {
+		$mysqli = User::connect();
+
+		$result = $mysqli->query("UPDATE User SET " .
+			"first=" .
+			"'" . $mysqli->real_escape_string($this->first) . "', " .
+			"last=" .
+			"'" . $mysqli->real_escape_string($this->last) . "', " .
+			"email=" .
+			"'" . $mysqli->real_escape_string($this->email) .
+			" where id=" . $this->id);
+		return $result;
+	}
+
+	public function delete() {
+		$mysqli = User::connect();
+		$mysqli->query("DELETE FROM User WHERE id = " . $this->id);
+	}
+
+	public function getJSON() {
+
+		$json_obj = array('id' => $this->id,
+			'first' => $this->first,
+			'last' => $this->last,
+			'email' => $this->email);
+		return json_encode($json_obj);
+	}
+
 }
 ?>
